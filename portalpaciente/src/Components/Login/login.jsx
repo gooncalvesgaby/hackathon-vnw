f129b0c0a5a511239836851815bce2a22ae626d7
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-06-06 19:00:24 -0300

    validaçao do login

--- portalpaciente/src/Components/Login/login.jsx
+++ portalpaciente/src/Components/Login/login.jsx
@@ -1,25 +1,59 @@
-import React from "react"
+import React, {useState} from "react"
 import Portal from '../../assets/bckportal.jpg'
 import * as S from "./loginStyle.jsx"
+import { useNavigate } from "react-router-dom";
+
+
 
 function Login() {
+
+    const [cpf, setCpf] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleCpf = (event) => {
+        setCpf(event.target.value.replace(/\D/, ''))
+    }
+
+    const handlePassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const loginForm = (event) => {
+        event.preventDefault()
+        if(cpf.length < 11 || !cpf || cpf == '' || cpf.length > 11) {
+            alert("informe um cpf valido");
+            return;
+        }
+
+        if(password == "") {
+            alert("digite sua senha")
+            return;
+        }
+
+        navigate('/portal')
+
+
+    }
+ 
+
     return (
         <>
        
         <main>
             <S.Section style={{ backgroundImage: `url(${Portal})` }}>
 
-                <S.Form>
+                <S.Form  onSubmit={loginForm}>
                     <h1>Acesse seu Login</h1>
                     <div>
-                        <input type="text" placeholder="CPF" />
+                        <input value={cpf} type="text" placeholder="CPF" onChange={handleCpf}/>
                     </div>
                     <div>
-                        <input type="password" placeholder="Senha" />
+                        <input value={password} type="password" placeholder="Senha" onChange={handlePassword}/>
                     </div>
                     <div>
                         <label>
-                            <input type="checkbox" />Lembre de mim
+                            <input  type="checkbox" />Lembre de mim
                         </label> 
                         <a href="#">Esqueceu a senha</a>
                     </div>
@@ -29,7 +63,7 @@ function Login() {
                             Não possui cadastro <a href="#">Cadastre-se</a>
                         </p>
                     </div>
-                    <button>Cadastre-se</button>
+                    <button type="submit">Cadastre-se</button>
                 </S.Form>
             </S.Section>
         </main>
