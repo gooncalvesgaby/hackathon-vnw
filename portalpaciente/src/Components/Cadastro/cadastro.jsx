c2d1f17b3566a7bcc4926a51d652b7ef209c51b1
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-06-06 20:34:04 -0300

    cadastro

--- portalpaciente/src/Components/Cadastro/cadastro.jsx
+++ portalpaciente/src/Components/Cadastro/cadastro.jsx
@@ -1,12 +1,33 @@
 import React from "react"
 import * as S from "./cadastroStyle.jsx"
+import Logo from "../../assets/logo-ds.png"
 
-function Cadastro() {
+ function Cadastro (){
     return(
-        <S.Div>
-            <S.H1>vamos ver</S.H1>
-        </S.Div>
-    )
-}
+        <S.Main>
+         <section>
+            <div>
+                <img src={Logo}/>
+                <h2>Cadastro do Usuario </h2>
 
-export default Cadastro
+            </div>
+         </section>
+         <S.Forms>
+         <h1>FORMULARIO CADASTRO </h1>
+          <label >NOME</label>
+          <input type="text" placeholder="Nome Completo"/>
+          <label >EMAIL</label>
+          <input type="text" placeholder="Email"/>
+          <label >CPF</label>
+          <input type="text" placeholder="CPF"/>
+          <label >SENHA</label>
+          <input type="password" placeholder="Senha"/>
+          <label >CONFIRMAR SENHA</label>
+          <input type="password" placeholder="Confirme sua senha "/>
+          <button>Cadastra-se</button> 
+         </S.Forms>
+        </S.Main>
+    )
+ }
+ 
+ export default Cadastro
