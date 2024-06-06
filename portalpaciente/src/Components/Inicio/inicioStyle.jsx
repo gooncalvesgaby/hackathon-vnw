316f93874c48fe7e308800973eb977337d0322e5
MonizaBritto <monyza2017@gmail.com>
2024-06-06 11:07:37 -0300

    Desenvolvimento da Header da Página de Inicio

---
+++ portalpaciente/src/Components/Inicio/inicioStyle.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const Header = styled.header`
+    width:100vw;
+    background-color: #78B4D6;
+    height:13vw;
+    display:flex;
+    justify-content: space-evenly;
+    align-items: center;
+    color: white;
+`
+
+export const Logo = styled.img`
+    width: 10rem;
+    height: 10rem;
+`
+
+export const Acesso = styled.img`
+    display: flex;
+    width: 20rem;
+    justify-content: space-around;
+`
+export const Icone = styled.img`
+    width: 3rem;
+    height: 3rem;
+    margin: 0.5rem;
+`
