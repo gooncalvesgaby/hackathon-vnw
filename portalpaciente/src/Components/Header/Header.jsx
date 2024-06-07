ab66cded04059050d7131befc17cb284cd4fa81f
MonizaBritto <monyza2017@gmail.com>
2024-06-07 01:10:34 -0300

    tentativa de menu no perfil

--- portalpaciente/src/Components/Header/Header.jsx
+++ portalpaciente/src/Components/Header/Header.jsx
@@ -6,16 +6,14 @@ import * as S from "./headerStyle.jsx"
 
 function Header() {
     return(
-        
          <S.Div>
             <S.Img src={Logo} alt="imagem de fundo" />
             <   S.H1>Central de atendimento</S.H1>
             <S.DivMenu>
                 <S.ImgMenu src={Ajuda} alt="imagem para auxilio" />
-                        <S.ImgMenu src={Perfil} alt="imagem com esboço de um perfil" />
+                            <S.ImgMenu src={Perfil} alt="imagem com esboço de um perfil" />
             </S.DivMenu>
         </S.Div>
-        
     )
 }
 
