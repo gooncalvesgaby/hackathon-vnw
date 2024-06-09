900653a5f47990a53f89f24479d081d2c1f59cb0
MariaClaraValotti <email do usurio>
2024-06-09 19:18:18 -0300

    conseguir ajeita uma parte do modal

--- portalpaciente/src/Components/Modal/modalstyle.jsx
+++ portalpaciente/src/Components/Modal/modalstyle.jsx
@@ -3,6 +3,13 @@ import styled from "styled-components"
 export const center = styled.section`
      display: flex;
      align-items: center;
+     @media(min-width: 320px) and (max-width: 768px){
+        margin-right: 5rem;
+        margin-bottom: 2rem;
+        flex-direction: column-reverse;
+        height: 15vh;
+     }
+     
 `
 
 export const Background = styled.div`
@@ -12,6 +19,9 @@ export const Background = styled.div`
     position: fixed;
     display: flex;
     color:#4677B6;
+    @media(min-width: 320px) and (max-width: 768px){
+        height: 50vh;
+    }
 `
 
 export const Conteiner = styled.div`
@@ -21,11 +31,17 @@ export const Conteiner = styled.div`
     background-color: white;
     box-shadow: black 0px 5px 15px;
     padding: 25px;
+    @media(min-width: 320px) and (max-width: 768px){
+        width: 60vw;
+        height: 45vh;
+       
+    }
 `
 
 export const selection = styled.div`
     display: flex;
     justify-content: space-evenly;
+
 `
 export const Title = styled.div`
     text-align: center;
@@ -52,15 +68,34 @@ export const option = styled.div`
     justify-content: space-around;
     margin-top: 1rem;
     text-decoration: underline #4677B6;
+    @media(min-width: 320px) and (max-width: 768px){
+        width: 30vw;
+        height: 15vh;
+        display: flex;
+        flex-direction: column-reverse;
+        justify-content: space-around;
+    }
 `
 export const grupo = styled.section`
     display: flex;
     justify-content: space-around;
+    @media(min-width: 320px) and (max-width: 768px){
+        height: 20vh;
+
+    }
+   
+
 `
 export const dados = styled.div`
     color: black;
     margin-top: 2rem;
     font-size: 1.3rem;
+    @media(min-width: 320px) and (max-width: 768px){
+        font-size: 0.6rem;
+        margin-bottom: 10rem;
+        
+    }
+    
 `
 export const button = styled.div`
     width: 7rem;
@@ -82,9 +117,13 @@ export const barra = styled.div`
 export const Input = styled.input`
     background-color: transparent;
     border: transparent;
+    @media(min-width: 320px) and (max-width: 768px){
+        width: 25vw;
+    }
 `
 
 export const Img = styled.img`
     width: 1.5rem;
     height: 1.5rem;
+   
 `
